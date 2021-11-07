136fc96d7ae6171aed854c414b7319006e08bd72
Ryan James Spencer <spencer.ryanjames@gmail.com>
2021-11-07 19:07:11 +1100

    Outfit snapshot tests for binaries

---
+++ lux-cli/src/bin/demo.rs
@@ -0,0 +1,10 @@
+use lux_core::data::{Chunk, Instruction};
+
+fn main() {
+    let mut chunk = Chunk::new();
+    chunk.write(Instruction::Return as u8, 123);
+    chunk.write(Instruction::Constant as u8, 123);
+    let constant = chunk.add_constant(1.2) as u8;
+    chunk.write(constant, 123);
+    chunk.dissassemble("test chunk");
+}
--- lux-cli/src/main.rs
+++ lux-cli/src/main.rs
@@ -1,11 +1,1 @@
-use lux_core::data::{Chunk, Instruction};
-
-// NOTE: just a demo for now.
-fn main() {
-    let mut chunk = Chunk::new();
-    chunk.write(Instruction::Return as u8, 123);
-    chunk.write(Instruction::Constant as u8, 123);
-    let constant = chunk.add_constant(1.2) as u8;
-    chunk.write(constant, 123);
-    chunk.dissassemble("test chunk");
-}
+fn main() {}
